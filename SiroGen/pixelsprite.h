077a33001b496de45a539d6afbbd9173fc8fd85a
SimonR <sw.roeloffs@hotmail.com>
2021-12-13 19:56:54 +0100

    Revert "Revert "Textures gone""
    This reverts commit cbd55de1bb6877abcd84b74e4dba35864f8b09fe.

--- SiroGen/pixelsprite.h
+++ SiroGen/pixelsprite.h
@@ -1,5 +1,16 @@
 #pragma once
+#include "sprite.h"
 class PixelSprite
 {
+public:
+	char width;
+	char height;
+	PixelSprite();
+	~PixelSprite();
+	GLuint sprite;
+	GLuint spritetexture;
+	GLuint uv;
+
+	void AddSprite(char canvas[], char width, char height);
 };
 
